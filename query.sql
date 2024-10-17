95269f677df160ad6a7f48fb8acf4811ce8c6b18
gaurihallale <87903557+gaurihallale@users.noreply.github.com>
2024-10-17 19:56:21 +0530

    Delete query.sql

--- query.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.user_id, u.username, SUM(o.amount) AS total_order_amount
-FROM users u
-JOIN orders o ON u.user_id = o.user_id
-WHERE o.order_date >= (CURRENT_DATE - INTERVAL '1 month')
-GROUP BY u.user_id, u.username
-ORDER BY total_order_amount DESC
-LIMIT 5;
